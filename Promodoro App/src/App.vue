e4fbd323e6c61422acbcdab0407f3e3335762ba0
AlexanderKudr <trevan98@gmail.com>
2022-08-13 23:40:00 +0300

    Starting and setting up the pj

---
+++ Promodoro App/src/App.vue
@@ -0,0 +1,53 @@
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+   <div class="card">
+      <p class="another">LOL</p>
+   </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+@import "../src/style.scss";
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
