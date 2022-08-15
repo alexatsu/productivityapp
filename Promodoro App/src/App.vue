fafd744f540169a8b9ce8bc635211305fc878502
AlexanderKudr <trevan98@gmail.com>
2022-08-15 23:01:03 +0300

    3 survived

--- Promodoro App/src/App.vue
+++ Promodoro App/src/App.vue
@@ -1,6 +1,33 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
+const seconds = ref(0);
+const display = ref("00 : 00");
+
+function formatCode(secs) {
+  const currentMinutes = Math.floor(secs / 60);
+  const currentSeconds = secs % 60;
+  return `${currentMinutes < 10 ? "0" + currentMinutes : currentMinutes}
+          : ${currentSeconds < 10 ? "0" + currentSeconds : currentSeconds}`;
+}
+function countdown() {
+  if (seconds.value >= 0) {
+    setTimeout(() => {
+      seconds.value--;
+      display.value = formatCode(seconds.value);
+      countdown();
+    }, 1000)
+  }
+}
+function addOneMinute() {
+  seconds.value = seconds.value + 60;
+  display.value = formatCode(seconds.value);
+}
+function addFiveMinutes() {
+  seconds.value = seconds.value + 300;
+  display.value = formatCode(seconds.value);
+}
 </script>
 
 <template>
@@ -12,7 +39,12 @@ import HelloWorld from './components/HelloWorld.vue'
   <main>
     <div class="app">
       <nav class="nav">LOL</nav>
-      <div class="timer"></div>
+      <div class="timer">
+        <button @click="countdown()" v-if="seconds > 0">Start</button>
+        <div>{{ display }}</div>
+        <button @click="addOneMinute">Add one minute:</button>
+        <button @click="addFiveMinutes">Add five minutes:</button>
+      </div>
     </div>
   </main>
 </template>
