55e7c5c1f2f139d9ca9f3ec9cb5054c64daa26cb
AlexanderKudr <trevan98@gmail.com>
2022-08-19 21:57:03 +0300

    todos finished

--- Promodoro App/src/components/Todo.vue
+++ Promodoro App/src/components/Todo.vue
@@ -1,12 +1,31 @@
 <script setup>
+import { ref } from 'vue'
 
+let id = 0;
+const newTodo = ref('');
+const todos = ref([]);
 
-
+function addTodo() {
+    todos.value.push({ id: id++, text: newTodo.value, completed: false });
+    newTodo.value = '';
+}
+function removeTodo(todo) {
+    todos.value = todos.value.filter((remove) => remove !== todo);
+}
 
 </script>
 
 <template>
-    <div>Todo</div>
+    <form @submit.prevent="addTodo()">
+        <input v-model="newTodo" placeholder="Tasks" required/>
+        <button>Add todo</button>
+    </form>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.text }}
+            <button @click="removeTodo(todo)"><input type="checkbox"/></button>
+        </li>
+    </ul>
 </template>
 
 <style scoped lang="scss">
