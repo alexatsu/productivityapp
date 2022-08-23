54f0664e05b2cc883c20e2366f8eef1611848852
AlexanderKudr <trevan98@gmail.com>
2022-08-23 18:54:47 +0300

    9 backup

--- Promodoro App/src/App.vue
+++ Promodoro App/src/App.vue
@@ -1,6 +1,28 @@
 <script setup>
 import Todo from './components/Todo.vue';
 import { ref } from 'vue';
+//fontawesome imports |
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faPlay,
+  faPause,
+  faArrowRotateRight,
+  faPlus,
+  fa1,
+  faM,
+  fa5,
+  fa0 } from '@fortawesome/free-solid-svg-icons'
+library.add(
+  faPlay, 
+  faPause, 
+  faArrowRotateRight, 
+  faPlus, 
+  fa1, 
+  fa5, 
+  fa0, 
+  faM,)
+//fontawesome imports |
 
 let timerRunning;
 let btnToggle = ref('Start');
@@ -64,18 +86,35 @@ function todoTimeEvent(secs) {
 </script>
 
 <template>
+
   <body>
     <header class="nav">Promodoro
     </header>
     <main class="main-app">
       <div class="countdown">
+        <div><span class="timer">{{ display }}</span></div>
         <button @click="countdownStart()" v-if="seconds > 0">{{ btnToggle }}</button>
         <button @click="countdownReset()" v-if="seconds > 0">Reset</button>
-        <div><span class="timer">{{ display }}</span></div>
-        <button @click="addOneMinute" v-if="seconds < 3600">Add one minute:</button>
-        <button @click="addFiveMinutes" v-if="seconds < 3360">Add five minutes:</button>
-        <button @click="addTenMinutes" v-if="seconds < 3060">Add ten minutes:</button>
+        <button @click="addOneMinute" v-if="seconds < 3600">
+          <font-awesome-icon icon="fa-solid fa-plus" transform='shrink-5' />
+          <font-awesome-icon icon="fa-solid fa-1" size='1x' />
+          <font-awesome-icon icon="fa-solid fa-m" transform='shrink-3' />
+        </button>
+        <button @click="addFiveMinutes" v-if="seconds < 3360">
+          <font-awesome-icon icon="fa-solid fa-plus" transform='shrink-5' />
+          <font-awesome-icon icon="fa-solid fa-5" size='1x' />
+          <font-awesome-icon icon="fa-solid fa-m" transform='shrink-3' />
+        </button>
+        <button @click="addTenMinutes" v-if="seconds < 3060">
+          <font-awesome-icon icon="fa-solid fa-plus" transform='shrink-5' />
+          <font-awesome-icon icon="fa-solid fa-1" size='1x' />
+          <font-awesome-icon icon="fa-solid fa-0" size='1x' />
+          <font-awesome-icon icon="fa-solid fa-m" transform='shrink-3' />
+        </button>
       </div>
+      <font-awesome-icon icon="fa-solid fa-play" size='2x' />
+      <font-awesome-icon icon="fa-solid fa-pause" size='2x' />
+      <font-awesome-icon icon="fa-solid fa-arrow-rotate-right" size='2x' rotation="180" />
       <Todo @testEvent="todoTimeEvent" />
     </main>
     <footer>Some links</footer>
@@ -85,3 +124,4 @@ function todoTimeEvent(secs) {
 <style scoped lang="scss">
 @use "../src/App.scss";
 </style>
+
